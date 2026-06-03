b2e00986f4d52df69b53b459c741f5173b808f1c
sumanth reddy <164669767+Sumanth0019@users.noreply.github.com>
2026-06-03 07:01:42 +0530

    Refactor supabase_client.py for type hinting

--- saas-analytics/backend/supabase_client.py
+++ saas-analytics/backend/supabase_client.py
@@ -1,14 +1,16 @@
-import os
 import streamlit as st
-from dotenv import load_dotenv
-from supabase import create_client
 
-load_dotenv()
+from supabase import (
+    create_client,
+    Client
+)
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
+
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
-supabase = create_client(
+supabase: Client = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
+
